89be996f18a52a8f9c9737b7f597d9c8344a3839
Coffie <coffie@pressplay.cc>
2022-10-27 13:01:00 +0800

    [Chg] 手機版調整

--- js/index.js
+++ js/index.js
@@ -1,7 +1,28 @@
+// choiceLang
+function choiceLang() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+  
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
+// banner swiper
 const swiper = new Swiper('.swiper', {
     loop: true,
 });
 
+// accordion
 var acc = document.getElementsByClassName("accordion");
 var i;
 for (i = 0; i < acc.length; i++) {
@@ -14,4 +35,31 @@ for (i = 0; i < acc.length; i++) {
             panel.style.maxHeight = panel.scrollHeight + "px";
         }
     });
+}
+
+// footer accordion
+var acc = document.getElementsByClassName("footer-accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
+
+// sidebar
+function openNav() {
+    document.getElementById("pal-sidebar").style.width = "250px";
+    // document.getElementById("main").style.marginLeft = "250px";
+}
+  
+function closeNav() {
+    document.getElementById("pal-sidebar").style.width = "0";
+    // document.getElementById("main").style.marginLeft= "0";
 }
