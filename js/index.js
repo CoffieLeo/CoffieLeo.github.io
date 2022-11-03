0cd9b6b1af34ad9eed38cb899198d6a1034e1cb0
Coffie <coffie@pressplay.cc>
2022-11-03 15:13:11 +0800

    [Chg] 調整比例問題

--- js/index.js
+++ js/index.js
@@ -1,22 +1,3 @@
-// choiceLang
-function choiceLang() {
-    document.getElementById("myDropdown").classList.toggle("show");
-}
-  
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-        var dropdowns = document.getElementsByClassName("dropdown-content");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-            }
-        }
-    }
-}
-
 // banner swiper
 const bannerSwiper = new Swiper('.swiper', {
     loop: true,
