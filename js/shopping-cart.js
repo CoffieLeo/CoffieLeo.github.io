d77ce81f22ec8295052d0f66c695ca473b26f475
Coffie <coffie@pressplay.cc>
2022-11-15 13:07:41 +0800

    [Chg] 修正購物車手機版

--- js/shopping-cart.js
+++ js/shopping-cart.js
@@ -3,3 +3,30 @@ document.body.style.paddingBottom = box.offsetHeight + 'px';
 addEventListener('resize', (event) => {
     document.body.style.paddingBottom = box.offsetHeight + 'px';
 });
+
+// toggleFreeBox
+function openFreeBox() {
+    document.querySelector('.choose-free-fixed').classList.add('active')
+}
+  
+function closeFreeBox() {
+    document.querySelector('.choose-free-fixed').classList.remove('active')
+}
+
+// toggleCouponBox
+function openCouponBox() {
+    document.querySelector('.choose-coupon-fixed').classList.add('active')
+}
+  
+function closeCouponBox() {
+    document.querySelector('.choose-coupon-fixed').classList.remove('active')
+}
+
+// toggleCouponBox
+function openAddressBox() {
+    document.querySelector('.choose-address-fixed').classList.add('active')
+}
+  
+function closeAddressBox() {
+    document.querySelector('.choose-address-fixed').classList.remove('active')
+}
