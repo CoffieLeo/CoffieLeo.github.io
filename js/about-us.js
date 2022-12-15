8c37c7866570f568c213e7f9a34d823b0e1555a8
Coffie <coffie@pressplay.cc>
2022-12-15 10:18:09 +0800

    [Chg] 修正 about-us

--- js/about-us.js
+++ js/about-us.js
@@ -2,6 +2,10 @@ var swiper = new Swiper(".skin-ai-swiper", {
     slidesPerView: 3,
     spaceBetween: 30,
     breakpoints: {
+        390: {
+            slidesPerView: 1,
+            spaceBetween: 30,
+        },
         767: {
             slidesPerView: 1,
             spaceBetween: 30,
