f285fc6c16581f56d30ccc6fe67ab1fd06ef7b7c
Coffie <coffie@pressplay.cc>
2022-12-22 13:25:31 +0800

    [Chg] 修正 new-member 頁面

--- js/new-member.js
+++ js/new-member.js
@@ -1,5 +1,5 @@
-// favorite-btn
-let target = document.querySelectorAll('.step-2 li');
+// is-choose
+let target = document.querySelectorAll('.step-2 li, .step-3 li');
 if (target) {
     for (let i = 0; i < target.length; i += 1) {
         const elem = target[i];
